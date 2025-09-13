1eb5ed08e32d8c6541bcbec6ed31111ad1ec4a02
miutaku <e79f85e2.miutaku.onmicrosoft.com@jp.teams.ms>
2025-09-14 02:04:06 +0900

    PVE(Proxmox VE)の環境をデプロイできるようにした (#3)
    * Add Terraform config for Proxmox-based RKE2 cluster infrastructure
    
    - Configure Proxmox VMs for RKE2 load balancers (HAProxy/Keepalived), servers, and worker nodes with sequential MAC addresses and VMIDs
    - Set up remote Terraform backend for state management in Proxmox workspace
    - Define VM resources including compute specs, network configs, and storage parameters
    - Add Kubernetes CoreDNS manifests and OCI VPN Terraform configurations
    - Implement variable-driven scaling through lb/server/worker VM counts and base MAC addresses
    
    * add: master,worker tag
    
    * ociのtfのworkflowを分離
    
    * feat: pveのterraform workflowを用意
    
    * feat: terraform cloudだからbackend.tfではなくてcloud.tf?
    
    * fix: pve cloud.tf workspace
    
    * pve向けtf workflowのファイルリネーム
    
    * pveのはdev/nullに捨てなくていい
    
    * fix: 最終行に改行入れ忘れてたので
    
    * fix: やっぱりworkflowのterraform実行結果をdev/nullに逃がすことにした
    
    * fix: 最終行に改行入れ忘れてたので_2
    
    * feat: VMをPVEのノードで分散できるようにした

---
+++ terraform/pve/variables.tf
@@ -0,0 +1,61 @@
+variable "pm_api_token_id" {
+  description = "Proxmox API Token ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "pm_api_token_secret" {
+  description = "Proxmox API Token Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "lb_vm_count" {
+    description = "The number of virtual machines"
+    type        = number
+    default     = 2
+}
+
+variable "server_vm_count" {
+    description = "The number of virtual machines"
+    type        = number
+    default     = 3
+}
+
+variable "worker_vm_count" {
+    description = "The number of virtual machines"
+    type        = number
+    default     = 2
+}
+
+variable "rke2_base_lb_macaddr" {
+    description = "The base MAC address of the virtual machines"
+    type        = string
+    default     = "BC:24:11:AD:44:00"
+}
+
+variable "rke2_base_server_macaddr" {
+    description = "The base MAC address of the virtual machines"
+    type        = string
+    default     = "BC:24:11:97:96:00"
+}
+
+variable "rke2_base_worker_macaddr" {
+    description = "The base MAC address of the virtual machines"
+    type        = string
+    default     = "BC:24:11:23:32:00"
+}
+
+#variable "tuner_vm_name" {
+#    description = "The name of the mirakurun virtual machine"
+#    type        = string
+#    default     = "mirakurun-ubuntu-24-04-home-amd64"
+#}
+
+variable "proxmox_nodes" {
+  description = "A list of Proxmox nodes to distribute VMs across."
+  type        = list(string)
+  default     = ["pve-x570"]
+#  default     = ["pve-x570", "pve-b550m"]
+}
+
