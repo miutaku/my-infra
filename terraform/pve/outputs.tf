1ecabab6c3e41911da7d1d119e0a555e2da9da59
miutaku <e79f85e2.miutaku.onmicrosoft.com@jp.teams.ms>
2025-09-23 19:13:02 +0900

    fix: 足りてないoutput追記

--- terraform/pve/outputs.tf
+++ terraform/pve/outputs.tf
@@ -1,3 +1,10 @@
+output "rke2_server_vm_names" {
+  value = [for vm in values(module.rke2_server.vms) : vm.name]
+}
+output "rke2_server_vm_ids" {
+  value = [for vm in values(module.rke2_server.vms) : vm.id]
+}
+
 output "rke2_lb_vm_names" {
   value = [for vm in values(module.rke2_lb.vms) : vm.name]
 }
