fdd776d84ada1b7b86e5bdf6639d5882c3e7a4c9
miutaku <e79f85e2.miutaku.onmicrosoft.com@jp.teams.ms>
2025-09-23 15:33:13 +0900

    feat: 録画サーバを定義 (#19)
    * feat: 録画サーバを定義
    
    * feat: dev環境も定義
    
    * fix: 本番の録画サーバのメモリサイズを修正

--- terraform/pve/main.tf
+++ terraform/pve/main.tf
@@ -40,70 +40,49 @@ module "rke2_worker" {
   proxmox_nodes = var.proxmox_nodes
 }
 
-#resource "proxmox_vm_qemu" "tuner" {
-#  # options
-#  vmid        = 30000
-#  protection  = false
-#  name        = var.tuner_vm_name
-#  agent       = 1 # qemu-guest-agent
-#  automatic_reboot = true
-#  onboot      = true
-#
-#  # hardware
-#  ## boot
-#  scsihw      = "virtio-scsi-single"
-#  bios        = "seabios"
-#  boot        = "order=scsi0"
-#  target_node = "pve-x570"
-#  clone       = "docker-ubuntu-24-04-home-amd64"
-#  full_clone  = false
-#
-#  ## cpu
-#  vcpus = 0 # this is set automatically by Proxmox to sockets * cores. https://registry.terraform.io/providers/Telmate/proxmox/latest/docs/resources/vm_qemu
-#  cores = 1
-#  sockets = 1
-#  cpu_type = "host"
-#
-#  ## memory
-#  memory = 9216
-#  balloon = 1
-#
-#  # network
-#  network {
-#    id = 0
-#    model  = "virtio"
-#    bridge = "vmbr0"
-#    firewall = false
-#    macaddr = "52:54:00:23:98:fc"
-#  }
-#
-#  # disk
-#  disks {
-#    scsi {
-#      scsi0 {
-#        disk {
-#          backup = true
-#          emulatessd = false
-#          size = "20G"
-#          storage = "local-zfs"
-#          iothread = true
-#          replicate = true
-#        }
-#      }
-#    }
-#  }
-#  # PCI device
-#  pcis {
-#    pci0 {
-#      mapping {
-#        mapping_id = "tuner_earthsoft"
-#        pcie = false
-#        primary_gpu = false
-#        rombar = false
-#        sub_device_id = ""
-#        sub_vendor_id = ""
-#      }
-#    }
-#  }
-#
-#}
+module "prd_rec_server" {
+  source = "./modules/proxmox_vm"
+
+  vm_count       = var.prd_rec_server_vm_count
+  name_prefix    = "prd-rec-server"
+  name_suffix    = "docker-ubuntu-24-04-home-amd64"
+  base_macaddr   = var.prd_rec_server_macaddr
+  vmid_start     = 30000
+  tags           = ["prd", "ubuntu_2404", "rec-server", "docker"]
+  cpu_cores      = 1
+  memory         = 4096
+  proxmox_nodes  = ["pve-b550m"] # PCI device is on a specific node
+  clone_template = "template-ubuntu-24-04-home-amd64"
+  disk_size      = 64
+  pcis = {
+    pci0 = {
+      mapping = {
+        mapping_id = "earthsoft_pt3"
+        pcie       = true
+      }
+    }
+  }
+}
+
+module "dev_rec_server" {
+  source = "./modules/proxmox_vm"
+
+  vm_count       = var.dev_rec_server_vm_count
+  name_prefix    = "dev-rec-server"
+  name_suffix    = "docker-ubuntu-24-04-home-amd64"
+  base_macaddr   = var.dev_rec_server_macaddr
+  vmid_start     = 31000
+  tags           = ["dev", "ubuntu_2404", "rec-server", "docker"]
+  cpu_cores      = 1
+  memory         = 2048
+  proxmox_nodes  = ["pve-x570"] # USB device is on a specific node
+  clone_template = "template-ubuntu-24-04-home-amd64"
+  disk_size      = 32
+  usbs = {
+    usb0 = {
+      mapping = {
+        mapping_id = "plex_s1ud"
+      }
+    }
+  }
+}
